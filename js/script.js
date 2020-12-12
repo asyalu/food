c3ac7787c1179f2aa43d0402598930bc751430d7
asyalu <fadin.egor.v@gmail.com>
2020-12-12 17:54:47 +0300

    create first commit

---
+++ js/script.js
@@ -0,0 +1,177 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i) {
+    tabsContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent(0);
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  const deadline = "2020-12-31";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
+
+  // modal window
+
+  const krest = document.querySelector("[data-close]"),
+    modal = document.querySelector(".modal"),
+    buttonConnect = document.querySelectorAll("[data-modal]");
+
+  krest.addEventListener("click", () => {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  });
+
+  buttonConnect.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden";
+      clearInterval(modalTimerID);
+    });
+  });
+
+  function closeModal() {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  }
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  function openModal() {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+
+  }
+
+  const modalTimerID = setTimeout(openModal, 6000);
+
+  function showModalByScroll () {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll)
+      }
+  }
+
+  window.addEventListener('scroll', showModalByScroll)
+});
+
+class MenuCard {
+  constructor(src, alt, title, descr, prace, perentSelector) {
+    this.src = src;
+    this.alt = alt;
+    this.title = title;
+    this.descr = descr;
+    this.prace = prace;
+    this.parant = document.querySelector(parentSelector);
+    this.transfer = 27;
+    this.changeToUAH();
+  }
+
+  changeToUAH() {
+    this.prace = this.prace * this.transfer;
+  }
+
+
+render () {
+  const element = document.createElement('div');
+  element.innerHTML = `
+  <div class="menu__item">
+      <img src=${this.src} alt=${this.alt}>
+      <h3 class="menu__item-subtitle">${this.title}</h3>
+      <div class="menu__item-descr">${descr}</div>
+      <div class="menu__item-divider"></div>
+      <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.prace}</span> грн/день</div>
+      </div>
+  </div   
+  `;
+  this.parent.append(element);
+};
+ 
+}
+
+new MenuCard(
+  "img/"
+)
+
+
+const 
